42f7d2db30954d818875cfb9646fc9657fe3e42f
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-18 13:29:27 +0530

    ADDED : Welcome message

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+/**
+ * Purpose - Feeding details of people in the address book
+ *
+ * @author - Aviral N
+ * @version - 1.0
+ * date     - 18-08-2021
+ */
+public class AddressBook {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
